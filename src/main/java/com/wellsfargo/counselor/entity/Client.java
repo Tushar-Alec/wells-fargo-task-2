d6cb187bb11c4a2f430c04212f14ffa6e239393f
Tushar-Alec <petermoor822@gmail.com>
2026-01-20 01:59:45 +0530

    Created client entity

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue()
+    private long clientId;
+}
